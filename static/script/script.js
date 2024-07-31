8940b4449ca974eccc657cee22524228e5555cf1
P-Worthington <pworthington17@gmail.com>
2024-07-31 17:54:32 +0000

    ammend indentations on js

--- static/script/script.js
+++ static/script/script.js
@@ -22,44 +22,44 @@ let year = date.getFullYear();
 let month = date.getMonth();
 
 function displayCalendar() {
-  const firstDay = new Date(year, month, 1);
+    const firstDay = new Date(year, month, 1);
 
-  const lastDay = new Date(year, month + 1, 0);
+    const lastDay = new Date(year, month + 1, 0);
 
-  const firstDayIndex = firstDay.getDay(); //4
+    const firstDayIndex = firstDay.getDay(); //4
 
-  const numberOfDays = lastDay.getDate(); //31
+    const numberOfDays = lastDay.getDate(); //31
 
-  let formattedDate = date.toLocaleString("en-US", {
-    month: "long",
-    year: "numeric"
-  });
+    let formattedDate = date.toLocaleString("en-US", {
+        month: "long",
+        year: "numeric"
+    });
 
-  display.innerHTML = `${formattedDate}`;
+    display.innerHTML = `${formattedDate}`;
 
-  for (let x = 1; x <= firstDayIndex; x++) {
-    const div = document.createElement("div");
-    div.innerHTML += "";
+    for (let x = 1; x <= firstDayIndex; x++) {
+        const div = document.createElement("div");
+        div.innerHTML += "";
 
-    days.appendChild(div);
-  }
-
-  for (let i = 1; i <= numberOfDays; i++) {
-    let div = document.createElement("div");
-    let currentDate = new Date(year, month, i);
-
-    div.dataset.date = currentDate.toDateString();
-
-    div.innerHTML += i;
-    days.appendChild(div);
-    if (
-      currentDate.getFullYear() === new Date().getFullYear() &&
-      currentDate.getMonth() === new Date().getMonth() &&
-      currentDate.getDate() === new Date().getDate()
-    ) {
-      div.classList.add("current-date");
+        days.appendChild(div);
     }
-  }
+
+    for (let i = 1; i <= numberOfDays; i++) {
+        let div = document.createElement("div");
+        let currentDate = new Date(year, month, i);
+
+        div.dataset.date = currentDate.toDateString();
+
+        div.innerHTML += i;
+        days.appendChild(div);
+        if (
+            currentDate.getFullYear() === new Date().getFullYear() &&
+            currentDate.getMonth() === new Date().getMonth() &&
+            currentDate.getDate() === new Date().getDate()
+        ) {
+            div.classList.add("current-date");
+        }
+    }
 }
 
 // Call the function to display the calendar
